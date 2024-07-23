9b671163b77780729c010c69c0fb090e77b3486a
Lets <ruvan123@naver.com>
2024-07-23 23:28:37 +0900

    Feat: UIButtonConfiguration을 이용하여 Buidler패턴처럼 Button Style 바꾸기

---
+++ MySplashed/MySplashed/Utils/Extensions/UIButton+.swift
@@ -0,0 +1,60 @@
+//
+//  UIButton+.swift
+//  MySplashed
+//
+//  Created by user on 7/23/24.
+//
+
+import UIKit
+
+extension UIButton {
+    var config: UIButton.Configuration {
+        return self.configuration ?? UIButton.Configuration.plain()
+    }
+    
+    func image(named: String) -> Self {
+        var config = config
+        config.image = UIImage(named: named)
+        self.configuration = config
+        return self
+    }
+    
+    func image(systemName: String) -> Self {
+        var config = config
+        config.image = UIImage(systemName: systemName)
+        self.configuration = config
+        return self
+    }
+    
+    func title(_ title: String) -> Self {
+        var config = config
+        config.attributedTitle = AttributedString(stringLiteral: title)
+        self.configuration = config
+        return self
+    }
+    
+    func backgroundColor(_ color: UIColor) -> Self {
+        var config = config
+        config.background.backgroundColor = color
+        self.configuration = config
+        return self
+    }
+    
+    func font(ofSize fontSize: CGFloat, weight: UIFont.Weight) -> Self {
+        var config = config
+        let transformer = UIConfigurationTextAttributesTransformer { oldValue in
+            var newValue = oldValue
+            newValue.font = UIFont.systemFont(ofSize: fontSize, weight: weight)
+            return newValue
+        }
+        config.titleTextAttributesTransformer = transformer
+        return self
+    }
+    
+    func cornerStyle(_ cornerStyle: Configuration.CornerStyle) -> Self {
+        var config = config
+        config.cornerStyle = cornerStyle
+        self.configuration = config
+        return self
+    }
+}
