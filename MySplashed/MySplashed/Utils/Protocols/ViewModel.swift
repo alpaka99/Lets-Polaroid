84e49b3fba60769aa1b9ab40f5226f8f4ff217e1
Lets <ruvan123@naver.com>
2024-07-23 20:50:35 +0900

    Fix: ViewModel 코드를 BaseViewModel에서 ViewModel protocol로 분리 및 gitignore 수정

--- MySplashed/MySplashed/Utils/Protocols/ViewModel.swift
+++ MySplashed/MySplashed/Utils/Protocols/ViewModel.swift
@@ -1,8 +1,8 @@
 //
-//  BaseViewModel.swift
+//  ViewModel.swift
 //  MySplashed
 //
-//  Created by user on 7/22/24.
+//  Created by user on 7/23/24.
 //
 
 import Foundation
@@ -24,9 +24,6 @@ protocol ViewModel<Input, Output>: AnyObject {
     // react 메서드의 내부는 switch-case로 구현되어 가독성을 높히고, 명확하게 action에 따른 logic을 실행 할 수 있게 함
     func react<U: Equatable>(_ action: Action, value: U)
 }
-
-protocol ViewModelState { }
-
 
 extension ViewModel {
     // callAsFunction의 경우, 정적으로 호출 가능한(statically callable value)의 값을 직접 호출할 수 있게 해줌
@@ -71,6 +68,10 @@ extension ViewModel {
 }
 
 
+
+/*
+    사용법(Sample Usage)
+ */
 final class BasicViewModel: ViewModel {
     struct Input: Equatable {
         let input = Observable("")
@@ -88,35 +89,33 @@ final class BasicViewModel: ViewModel {
     }
     
     func react<T>(_ action: Action, value: T) where T : Equatable {
-//        switch action {
-//        case .basicAction:
-//            reduce(\.input.value, into: "sad")
-//        }
+        switch action {
+        case .basicAction:
+            reduce(\.input.value, into: "qwer")
+        }
     }
 }
 
-/*
- 사용법(Usage):
- */
+
 class TestClass {
     let viewModel = BasicViewModel()
     
     func test() {
-        /*
-        extension이 BaseViewModel에선언되어 있어서 baseViewModel의 keyPath만 읽어오는 문제
-         그렇다고 input과 output을 protocol에 명시해놓으면 접근을 할 수 있게 되어버리는데...
-         private(set)으로 읽기는 가능하지만 쓰기는 못하게 해야하려나...
-         더불어서 reduce() 메서드도 접근 가능하네 흠... react만 접근 가능해져야하는데 말이지...
-         근데 bind는 그냥 가능하게 하고 싶으니까 일단 private(set)으로 해놓고, reduce는 BaseViewModel의 메서드로 구현해놔야할듯하네요?
-         -> private(set)으로 구현하면 WritableKeyPath가 아닌, 그냥 KeyPath 타입으로 추론되서 값을 가져오기는 가능한데 값을 넣는 reduce메서드를 구현할 수 없음 흠...
-         */
         viewModel.react(.basicAction, value: "123")
-        viewModel.reduce(\.input.value, into: "456")
         viewModel(\.input).bind { _ in
             // bind closure
         }
     }
 }
+
+/*
+extension이 BaseViewModel에선언되어 있어서 baseViewModel의 keyPath만 읽어오는 문제
+ 그렇다고 input과 output을 protocol에 명시해놓으면 접근을 할 수 있게 되어버리는데...
+ private(set)으로 읽기는 가능하지만 쓰기는 못하게 해야하려나...
+ 더불어서 reduce() 메서드도 접근 가능하네 흠... react만 접근 가능해져야하는데 말이지...
+ 근데 bind는 그냥 가능하게 하고 싶으니까 일단 private(set)으로 해놓고, reduce는 BaseViewModel의 메서드로 구현해놔야할듯하네요?
+ -> private(set)으로 구현하면 WritableKeyPath가 아닌, 그냥 KeyPath 타입으로 추론되서 값을 가져오기는 가능한데 값을 넣는 reduce메서드를 구현할 수 없음 흠...
+ */
 
 
 // MARK: WritableKeyPath Problem
