d8ef0aff20af1c42239ded0c2908333248559b36
Lets <ruvan123@naver.com>
2024-07-27 22:57:04 +0900

    Feat: RealmManager 생성

---
+++ MySplashed/MySplashed/Utils/Managers/RealmManager.swift
@@ -0,0 +1,31 @@
+//
+//  RealmManager.swift
+//  MySplashed
+//
+//  Created by user on 7/27/24.
+//
+
+import RealmSwift
+
+final class RealmManager {
+    static let shared = RealmManager()
+    private let realm = try! Realm()
+    
+    private init() { }
+    
+    func create<T: Object>(_ data: T) throws {
+        try realm.write {
+            realm.add(data)
+        }
+    }
+    
+    func readAll<T: Object>(_ dataType: T.Type) throws -> [T] {
+        return Array(realm.objects(dataType.self))
+    }
+    
+    func delete<T: Object>(_ data: T) throws {
+        try realm.write {
+            realm.delete(data)
+        }
+    }
+}
